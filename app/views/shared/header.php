e0216fb806192b76524d44cd698576b544c1eb7a
James <james_mcdonnell@hotmail.com>
2017-04-03 03:29:46 -0700

    Added link to admin page.

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -1,18 +1,24 @@
 <?php
 global $user;
+global $sub_path; 
  ?>
 	<header>
-		<h1><a href="<?php global $sub_path; echo $sub_path; ?>/">MyDiscussionForum</a></h1>
+		<h1><a href="<?php echo $sub_path; ?>/">MyDiscussionForum</a></h1>
 		<nav>
 			<ul>
-				<li><a href="<?php global $sub_path; echo $sub_path; ?>/">Home</a></li>
-				<li><a href="<?php global $sub_path; echo $sub_path; ?>/content/search">Search</a></li>
+				<li><a href="<?php echo $sub_path; ?>/">Home</a></li>
+
+				<?php if(!empty($user['admin']) && $user['admin']) { ?>
+					<li><a href="<?php echo $sub_path; ?>/content/administrate">Admin</a></li>
+				<?php } ?>
+
+				<li><a href="<?php echo $sub_path; ?>/content/search">Search</a></li>
 				<?php if(!isset($user)) { ?>
-					<li><a href="<?php global $sub_path; echo $sub_path; ?>/account/login">Login</a></li>
-					<li><a href="<?php global $sub_path; echo $sub_path; ?>/account/register">Register</a></li>
+					<li><a href="<?php echo $sub_path; ?>/account/login">Login</a></li>
+					<li><a href="<?php echo $sub_path; ?>/account/register">Register</a></li>
 				<?php } else { ?>
-					<li><a href="<?php global $sub_path; echo $sub_path; ?>/account/profile">Profile</a></li>
-					<li><a href="<?php global $sub_path; echo $sub_path; ?>/account/logout">Logout</a></li>
+					<li><a href="<?php echo $sub_path; ?>/account/profile">Profile</a></li>
+					<li><a href="<?php echo $sub_path; ?>/account/logout">Logout</a></li>
 				<?php } ?>
 			</ul>
 		</nav>
